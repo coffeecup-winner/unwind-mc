cafb24f72c828e5a37794a370c258dcaef8aff99
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2017-02-04 00:04:59 +0100

    Add flow tree body to the function

--- src/UnwindMC/Analysis/Function.cs
+++ src/UnwindMC/Analysis/Function.cs
@@ -1,7 +1,14 @@
-﻿namespace UnwindMC.Analysis
+﻿using System;
+using System.Collections.Generic;
+using UnwindMC.Analysis.Flow;
+using UnwindMC.Analysis.IL;
+
+namespace UnwindMC.Analysis
 {
     public class Function
     {
+        private List<IBlock> _blocks;
+
         public Function(ulong address)
         {
             Address = address;
@@ -10,6 +17,43 @@
 
         public ulong Address { get; }
         public FunctionStatus Status { get; set; }
+        public IReadOnlyList<IBlock> Blocks => _blocks;
+        public ILInstruction FirstInstruction
+        {
+            get
+            {
+                if (_blocks == null || _blocks.Count == 0)
+                {
+                    return null;
+                }
+                var seq = _blocks[0] as SequentialBlock;
+                if (seq != null)
+                {
+                    return seq.Instructions[0];
+                }
+                var loop = _blocks[0] as LoopBlock;
+                if (loop != null)
+                {
+                    return loop.Condition;
+                }
+                var cond = _blocks[0] as ConditionalBlock;
+                if (cond != null)
+                {
+                    return cond.Condition;
+                }
+                throw new InvalidOperationException("Unknown block type");
+            }
+        }
+
+        public void ResolveBody(InstructionGraph graph)
+        {
+            if (Status != FunctionStatus.BoundsResolved)
+            {
+                throw new InvalidOperationException("Cannot resolve function body when bounds are not resolved");
+            }
+            _blocks = FlowAnalyzer.Analyze(ILDecompiler.Decompile(graph, Address));
+            Status = FunctionStatus.BodyResolved;
+        }
     }
 
     public enum FunctionStatus
@@ -18,5 +62,6 @@
         BoundsResolved,
         BoundsNotResolvedInvalidAddress,
         BoundsNotResolvedIncompleteGraph,
+        BodyResolved,
     }
 }
