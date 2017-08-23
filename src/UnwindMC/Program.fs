2bf365df9ee9caebc2a3eab57710494ee6ffaf8e
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2017-08-23 23:24:09 +0200

    Start rewriting the app in F#

---
+++ src/UnwindMC/Program.fs
@@ -0,0 +1,10 @@
+﻿open UnwindMC.Decompilation
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| projectRootPath |] ->
+        let decompiler = new Decompiler(DecompilationProject.Load(projectRootPath))
+        decompiler.Decompile()
+    | _ -> printfn "Usage: unwind-mc <project-root-path>"
+    0
