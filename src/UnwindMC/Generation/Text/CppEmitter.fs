6c5af9e29eef2cfb9e2c25e12251e025ce0abed9
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2017-09-02 23:42:12 +0200

    Introduce text workflow

--- src/UnwindMC/Generation/Text/CppEmitter.fs
+++ src/UnwindMC/Generation/Text/CppEmitter.fs
@@ -2,8 +2,8 @@
 
 open System
 open System.Collections.Generic
-open System.Text
 open Ast
+open TextWorkflow
 open Type
 
 let private findRootVar (stmts: IReadOnlyList<Statement>): Var option =
@@ -29,227 +29,246 @@ let private findRootVar (stmts: IReadOnlyList<Statement>): Var option =
 let IndentSize = 2
 
 type private T = {
-    sb: StringBuilder
     name: string
     types: IReadOnlyDictionary<string, DataType>
     parametersCount: int
     body: IReadOnlyList<Statement>
     declaredVariables: HashSet<string>
-    indents: Dictionary<int, string>
-    mutable indentLevel: int
-    mutable indent: string
 }
 
 let emit (name: string) (types: IReadOnlyDictionary<string, DataType>) (parametersCount: int) (body: IReadOnlyList<Statement>): string =
     let t = {
-        sb = new StringBuilder()
         name = name
         types = types
         parametersCount = parametersCount
         body = body
         declaredVariables = new HashSet<string>()
-        indents = new Dictionary<int, string>()
-        indentLevel = 0
-        indent = ""
     }
-    t.indents.Add(0, "")
-    emitSourceCode t
+    text {
+        yield! emitSignature t (findRootVar t.body)
+        yield! emitScope t t.body false
+    } |> buildText { indentSize = 2 }
 
-let private emitSourceCode (t: T): string =
-    emitSignature t (findRootVar t.body)
-    emitScope t t.body false
-    t.sb.ToString()
+let private emitSignature (t: T) (ret: Var option): TextWorkflow.T =
+    text {
+        yield! emitType t ret
+        yield t.name
+        yield "("
+        for i in [0 .. t.parametersCount - 1] do
+            if i <> 0 then
+                yield ", "
+            yield! emitDeclaration t ("arg" + string(i)) // TODO: move argument names to function
+        yield ")"
+        yield NewLine
+    }
 
-let private emitSignature (t: T) (ret: Var option): unit =
-    emitType t ret
-    t.sb.Append(t.name)
-        .Append("(") |> ignore
-    for i in [0 .. t.parametersCount - 1] do
-        if i <> 0 then
-            t.sb.Append(", ") |> ignore
-        emitDeclaration t ("arg" + string(i)) // TODO: move argument names to function
-    t.sb.Append(")")
-        .Append(Environment.NewLine) |> ignore
+let private emitType (t: T) (var: Var option): TextWorkflow.T =
+    text {
+        match var with
+        | None ->
+            yield "void "
+        | Some (Var name) ->
+            let type_ = t.types.[name]
+            if type_.isFunction then
+                raise (new NotImplementedException())
+            yield "int "
+            yield new System.String('*', type_.indirectionLevel)
+    }
 
-let private emitType (t: T) (var: Var option): unit =
-    match var with
-    | None -> t.sb.Append("void ") |> ignore
-    | Some (Var name) ->
+let private emitDeclaration (t: T) (name: string): TextWorkflow.T =
+    text {
         let type_ = t.types.[name]
         if type_.isFunction then
-            raise (new NotImplementedException())
+            yield "void"
+            yield " "
+            yield "("
+            yield new System.String('*', type_.indirectionLevel + 1)
+            yield name
+            yield ")"
+            yield "()"
         else
-            t.sb.Append("int ")
-                .Append(new System.String('*', type_.indirectionLevel)) |> ignore
+            yield "int "
+            yield new System.String('*', type_.indirectionLevel)
+            yield name
+    }
 
-let private emitDeclaration (t: T) (name: string): unit =
-    let type_ = t.types.[name]
-    if type_.isFunction then
-        t.sb.Append("void")
-            .Append(" ")
-            .Append("(")
-            .Append(new System.String('*', type_.indirectionLevel + 1))
-            .Append(name)
-            .Append(")")
-            .Append("()") |> ignore
-    else
-        t.sb.Append("int ")
-            .Append(new System.String('*', type_.indirectionLevel))
-            .Append(name) |> ignore
+let private emitStatement (t: T) (statement: Statement): TextWorkflow.T =
+    text {
+        yield!
+            match statement with
+            | Assignment (var, expr) -> emitAssignment t var expr
+            | DoWhile (loop, cond) -> emitDoWhile t loop cond
+            | FunctionCall expr -> emitFunctionCall t expr
+            | IfThenElse (cond, trueBranch, falseBranch) -> emitIfThenElse t cond trueBranch falseBranch
+            | Return var -> emitReturn t var
+            | While (cond, loop) -> emitWhile t cond loop
+    }
 
-let private emitStatement (t: T) (statement: Statement): unit =
-    match statement with
-    | Assignment (var, expr) -> emitAssignment t var expr
-    | DoWhile (loop, cond) -> emitDoWhile t loop cond
-    | FunctionCall expr -> emitFunctionCall t expr
-    | IfThenElse (cond, trueBranch, falseBranch) -> emitIfThenElse t cond trueBranch falseBranch
-    | Return var -> emitReturn t var
-    | While (cond, loop) -> emitWhile t cond loop
+let private emitAssignment (t :T) (var: Var) (expr: Expression): TextWorkflow.T =
+    text {
+        let (Var name) = var
+        yield Indent
+        // TODO: rework how variables are passed and treated in each step
+        if not (name.StartsWith("arg")) && t.declaredVariables.Add(name) then
+            yield! emitDeclaration t name
+        else
+            yield name
+        yield " = "
+        yield! emitExpression t expr
+        yield ";"
+        yield NewLine
+    }
 
-let private emitAssignment (t :T) (var: Var) (expr: Expression): unit =
-    let (Var name) = var
-    t.sb.Append(t.indent) |> ignore
-    // TODO: reword how variables are passed and treated in each step
-    if not (name.StartsWith("arg")) && t.declaredVariables.Add(name) then
-        emitDeclaration t name
-    else
-        t.sb.Append(name) |> ignore
-    t.sb.Append(" = ") |> ignore
-    emitExpression t expr
-    t.sb.Append(";")
-        .Append(Environment.NewLine) |> ignore
+let private emitDoWhile (t: T) (loop: IReadOnlyList<Statement>) (cond: Expression): TextWorkflow.T =
+    text {
+        yield Indent
+        yield "do"
+        yield NewLine
 
-let private emitDoWhile (t: T) (loop: IReadOnlyList<Statement>) (cond: Expression): unit =
-    t.sb.Append(t.indent)
-        .Append("do")
-        .Append(Environment.NewLine) |> ignore
-    emitScope t loop true
-    t.sb.Append(" ")
-        .Append("while")
-        .Append(" ")
-        .Append("(") |> ignore
-    emitExpression t cond
-    t.sb.Append(")")
-        .Append(";")
-        .Append(Environment.NewLine) |> ignore
+        yield! emitScope t loop true
+        yield " "
+        yield "while"
+        yield " "
+        yield "("
+        yield! emitExpression t cond
+        yield ")"
+        yield ";"
+        yield NewLine
+    }
 
-let private emitFunctionCall (t: T) (expr: Expression): unit =
-    t.sb.Append(t.indent) |> ignore
-    emitExpression t expr
-    t.sb.Append("()")
-        .Append(";")
-        .Append(Environment.NewLine) |> ignore
+let private emitFunctionCall (t: T) (expr: Expression): TextWorkflow.T =
+    text {
+        yield Indent
+        yield! emitExpression t expr
+        yield "()"
+        yield ";"
+        yield NewLine
+    }
 
-let private emitIfThenElse (t: T) (cond: Expression) (trueBranch: IReadOnlyList<Statement>) (falseBranch: IReadOnlyList<Statement>): unit =
-    t.sb.Append(t.indent)
-        .Append("if")
-        .Append(" ")
-        .Append("(") |> ignore
-    emitExpression t cond
-    t.sb.Append(")")
-        .Append(Environment.NewLine) |> ignore
-    emitScope t trueBranch false
-    if falseBranch.Count > 0 then
-        t.sb.Append(t.indent)
-            .Append("else")
-            .Append(Environment.NewLine) |> ignore
-        emitScope t falseBranch false
+let private emitIfThenElse (t: T) (cond: Expression) (trueBranch: IReadOnlyList<Statement>) (falseBranch: IReadOnlyList<Statement>): TextWorkflow.T =
+    text {
+        yield Indent
+        yield "if"
+        yield " "
+        yield "("
+        yield! emitExpression t cond
+        yield ")"
+        yield NewLine
 
-let private emitReturn (t: T) (var: Var option): unit =
-    t.sb.Append(t.indent)
-        .Append("return") |> ignore
-    match var with
-    | Some (Var name) ->
-        t.sb.Append(" ")
-            .Append(name) |> ignore
-    | _ -> ()
-    t.sb.Append(";")
-        .Append(Environment.NewLine) |> ignore
+        yield! emitScope t trueBranch false
+        if falseBranch.Count > 0 then
+            yield Indent
+            yield "else"
+            yield NewLine
+            yield! emitScope t falseBranch false
+    }
 
-let private emitScope (t: T) (stmts: IReadOnlyList<Statement>) (skipNewline: bool): unit =
-    t.sb.Append(t.indent)
-        .Append("{")
-        .Append(Environment.NewLine) |> ignore
-    t.indentLevel <- t.indentLevel + 1
-    let hasValue, indent = t.indents.TryGetValue(t.indentLevel)
-    if hasValue then
-        t.indent <- indent
-    else
-        t.indent <- new System.String(' ', t.indentLevel * IndentSize)
-        t.indents.[t.indentLevel] <- t.indent
+let private emitReturn (t: T) (var: Var option): TextWorkflow.T =
+    text {
+        yield Indent
+        yield "return"
+        match var with
+        | Some (Var name) ->
+            yield " "
+            yield name
+        | _ -> ()
+        yield ";"
+        yield NewLine
+    }
 
-    for node in stmts do
-        emitStatement t node
+let private emitScope (t: T) (stmts: IReadOnlyList<Statement>) (skipNewline: bool): TextWorkflow.T =
+    text {
+        yield Indent
+        yield "{"
+        yield NewLine
 
-    t.indentLevel <- t.indentLevel - 1
-    t.indent <- t.indents.[t.indentLevel]
-    t.sb.Append(t.indent)
-        .Append("}") |> ignore
-    if not skipNewline then
-        t.sb.Append(Environment.NewLine) |> ignore
+        yield IncreaseIndent
+        for node in stmts do
+            yield! emitStatement t node
+        yield DecreaseIndent
 
-let private emitWhile (t: T) (cond: Expression) (loop: IReadOnlyList<Statement>): unit =
-    t.sb.Append(t.indent)
-        .Append("while")
-        .Append(" ")
-        .Append("(") |> ignore
-    emitExpression t cond
-    t.sb.Append(")")
-        .Append(Environment.NewLine) |> ignore
-    emitScope t loop false
+        yield Indent
+        yield "}"
+        if not skipNewline then
+            yield NewLine
+    }
 
-let private emitExpression (t: T) (expression: Expression): unit =
-    match expression with
-    | Binary (op, left, right) -> emitBinary t op left right
-    | Dereference expr -> emitDereference t expr
-    | Unary (op, operand) -> emitUnary t op operand
-    | Value value -> emitValue t value
-    | VarRef var -> emitVar t var
+let private emitWhile (t: T) (cond: Expression) (loop: IReadOnlyList<Statement>): TextWorkflow.T =
+    text {
+        yield Indent
+        yield "while"
+        yield " "
+        yield "("
+        yield! emitExpression t cond
+        yield ")"
+        yield NewLine
 
-let private emitBinary (t: T) (op: Operator) (left: Expression) (right: Expression): unit =
-    emitExpression t left
-    t.sb.Append(" ") |> ignore
-    let opString =
-        match op with
-        | Operator.Equal -> "=="
-        | Operator.NotEqual -> "!="
-        | Operator.Less -> "<"
-        | Operator.LessOrEqual -> "<="
-        | Operator.Greater -> ">"
-        | Operator.GreaterOrEqual -> ">="
-        | Operator.And -> "&"
-        | Operator.Or -> "|"
-        | Operator.Xor -> "^"
-        | Operator.ShiftLeft -> "<<"
-        | Operator.ShiftRight -> ">>"
-        | Operator.Add -> "+"
-        | Operator.Subtract -> "-"
-        | Operator.Multiply -> "*"
-        | Operator.Divide -> "/"
-        | Operator.Modulo -> "%"
-        | _ -> raise (new NotSupportedException())
-    t.sb.Append(opString)
-        .Append(" ") |> ignore
-    emitExpression t right
+        yield! emitScope t loop false
+    }
 
-let private emitDereference (t: T) (expr: Expression): unit =
-    t.sb.Append("*")
-        .Append("(") |> ignore
-    emitExpression t expr
-    t.sb.Append(")") |> ignore
+let private emitExpression (t: T) (expression: Expression): TextWorkflow.T =
+    text {
+        yield!
+            match expression with
+            | Binary (op, left, right) -> emitBinary t op left right
+            | Dereference expr -> emitDereference t expr
+            | Unary (op, operand) -> emitUnary t op operand
+            | Value value -> emitValue t value
+            | VarRef var -> emitVar t var
+    }
 
-let private emitUnary (t: T) (op: Operator) (operand: Expression): unit =
-    let opString =
-        match op with
-        | Operator.Negate -> "-"
-        | Operator.Not -> "~"
-        | _ -> raise (new NotSupportedException())
-    t.sb.Append(opString) |> ignore
-    emitExpression t operand
+let private emitBinary (t: T) (op: Operator) (left: Expression) (right: Expression): TextWorkflow.T =
+    text {
+        yield! emitExpression t left
+        yield " "
+        yield
+            match op with
+            | Operator.Equal -> "=="
+            | Operator.NotEqual -> "!="
+            | Operator.Less -> "<"
+            | Operator.LessOrEqual -> "<="
+            | Operator.Greater -> ">"
+            | Operator.GreaterOrEqual -> ">="
+            | Operator.And -> "&"
+            | Operator.Or -> "|"
+            | Operator.Xor -> "^"
+            | Operator.ShiftLeft -> "<<"
+            | Operator.ShiftRight -> ">>"
+            | Operator.Add -> "+"
+            | Operator.Subtract -> "-"
+            | Operator.Multiply -> "*"
+            | Operator.Divide -> "/"
+            | Operator.Modulo -> "%"
+            | _ -> raise (new NotSupportedException())
+        yield " "
+        yield! emitExpression t right
+    }
 
-let private emitValue (t: T) (value: int): unit =
-    t.sb.Append(value) |> ignore
+let private emitDereference (t: T) (expr: Expression): TextWorkflow.T =
+    text {
+        yield "*"
+        yield "("
+        yield! emitExpression t expr
+        yield ")"
+    }
 
-let private emitVar (t: T) (var: Var): unit =
-    let (Var name) = var
-    t.sb.Append(name) |> ignore
+let private emitUnary (t: T) (op: Operator) (operand: Expression): TextWorkflow.T =
+    text {
+        yield
+            match op with
+            | Operator.Negate -> "-"
+            | Operator.Not -> "~"
+            | _ -> raise (new NotSupportedException())
+        yield! emitExpression t operand
+    }
+
+let private emitValue (t: T) (value: int): TextWorkflow.T =
+    text {
+        yield value.ToString()
+    }
+
+let private emitVar (t: T) (var: Var): TextWorkflow.T =
+    text {
+        let (Var name) = var
+        yield name
+    }
---
+++ src/UnwindMC/Util/TextWorkflow.fs
@@ -0,0 +1,102 @@
+﻿module TextWorkflow
+
+open System
+open System.Collections.Generic
+open System.Text
+
+type Settings = {
+    indentSize: int
+}
+
+type State = {
+    settings: Settings
+    indentCache: Map<int, string>
+    indentLevel: int
+    indent: string
+}
+
+type T =
+    T of (StringBuilder -> State -> State)
+
+type TextControl
+    = IncreaseIndent
+    | DecreaseIndent
+    | Indent
+    | NewLine
+
+type TextBuilder() =
+    member self.Combine(T fa, T fb): T =
+        T (fun sb state ->
+            let state = fa sb state
+            fb sb state)
+
+    member self.Delay(f) =
+        f ()
+
+    member self.For(values: 'a seq, func: 'a -> T): T =
+        T (fun sb state ->
+            use enumerator = values.GetEnumerator()
+            let mutable st = state
+            while enumerator.MoveNext() do
+                match func enumerator.Current with
+                | T f -> st <- f sb st
+            st)
+
+    member self.Yield(v: string): T =
+        T (fun sb state ->
+            sb.Append(v) |> ignore
+            state)
+
+    member self.Yield(c: TextControl): T =
+        match c with
+        | IncreaseIndent ->
+            T (fun _ state ->
+                let newLevel = state.indentLevel + 1
+                match state.indentCache.TryFind(newLevel) with
+                | Some indent ->
+                    { state with
+                        indentLevel = newLevel
+                        indent = indent
+                    }
+                | None ->
+                    let indent = new System.String(' ', newLevel * state.settings.indentSize)
+                    { state with
+                        indentCache = state.indentCache.Add (newLevel, indent)
+                        indentLevel = newLevel
+                        indent = indent
+                    })
+        | DecreaseIndent ->
+            T (fun _ state ->
+                let newLevel = state.indentLevel - 1
+                { state with
+                    indentLevel = newLevel
+                    indent = state.indentCache.[newLevel]
+                })
+        | Indent ->
+            T (fun sb state ->
+                sb.Append(state.indent) |> ignore
+                state)
+        | NewLine ->
+            T (fun sb state ->
+                sb.Append(Environment.NewLine) |> ignore
+                state)
+
+    member self.YieldFrom(t: T): T =
+        t
+
+    member self.Zero(): T =
+        T (fun _ state -> state)
+
+let text = new TextBuilder()
+
+let buildText (settings: Settings) (t: T): string =
+    let sb = new StringBuilder()
+    let st = {
+        settings = settings
+        indentCache = Map<int, string> [0, ""]
+        indentLevel = 0
+        indent = ""
+    }
+    match t with
+    | T func -> func sb st |> ignore
+    sb.ToString()
