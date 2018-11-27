e70183a9009355d1ade47e25c8d2b05a5e2ce844
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2018-11-26 23:12:11 -0800

    Add the remaining arithmetic tests

--- tests/int_arithmetic_tests.rs
+++ tests/int_arithmetic_tests.rs
@@ -27,3 +27,215 @@ fn add() {
         ";
     test_decompiler(code, expected);
 }
+
+#[test]
+fn subtract() {
+    let code = "
+        int subtract(int a, int b) {
+          return a - b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg0;
+          var0 = var0 - arg1;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+#[test]
+fn negate() {
+    let code = "
+        int negate(int a) {
+          return -a;
+        }";
+    let expected = "
+        int sub_000000(int arg0)
+        {
+          int var0;
+        
+          var0 = arg0;
+          var0 = -var0;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+#[test]
+fn multiply() {
+    let code = "
+        int multiply(int a, int b) {
+          return a * b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg1;
+          var0 = var0 * arg0;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+#[test]
+fn divide() {
+    let code = "
+        int divide(int a, int b) {
+          return a / b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg0;
+          var0 = var0 / arg1;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+// #[test]
+// TODO: support multiple-output instructions
+fn modulo() {
+    let code = "
+        int divide(int a, int b) {
+          return a % b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg0;
+          var0 = var0 % arg1;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+#[test]
+fn not() {
+    let code = "
+        int _not(int a) {
+          return ~a;
+        }";
+    let expected = "
+        int sub_000000(int arg0)
+        {
+          int var0;
+        
+          var0 = arg0;
+          var0 = ~var0;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+#[test]
+fn and() {
+    let code = "
+        int _and(int a, int b) {
+          return a & b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg1;
+          var0 = var0 & arg0;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+#[test]
+fn or() {
+    let code = "
+        int _or(int a, int b) {
+          return a | b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg1;
+          var0 = var0 | arg0;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+#[test]
+fn xor() {
+    let code = "
+        int _xor(int a, int b) {
+          return a ^ b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg1;
+          var0 = var0 ^ arg0;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+// #[test]
+// TODO: support word registers
+fn left_shift() {
+    let code = "
+        int xor(int a, int b) {
+          return a << b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg0;
+          var0 = var0 << arg1;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
+
+// #[test]
+// TODO: support word registers
+fn right_shift() {
+    let code = "
+        int xor(int a, int b) {
+          return a >> b;
+        }";
+    let expected = "
+        int sub_000000(int arg0, int arg1)
+        {
+          int var0;
+        
+          var0 = arg0;
+          var0 = var0 >> arg1;
+          return var0;
+        }
+        ";
+    test_decompiler(code, expected);
+}
